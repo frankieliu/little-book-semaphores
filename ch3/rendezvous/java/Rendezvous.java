2e750f69df84bee6793018bce6e8cc5e885c56fe
Johny Rufus <johnjoh@iu.edu>
2019-07-04 11:43:46 -0700

    Rendezvous java

---
+++ ch3/rendezvous/java/Rendezvous.java
@@ -0,0 +1,43 @@
+package com.jrufus.conc;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by rufus on 7/4/19.
+ */
+public class Rendezvous {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        CountDownLatch latch = new CountDownLatch(2);
+        executorService.submit(() -> {
+            System.out.println("Executing a1");
+            latch.countDown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Executing a2");
+        });
+
+        executorService.submit(() -> {
+            System.out.println("Executing b1");
+            latch.countDown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Executing b2");
+        });
+
+        executorService.shutdown();
+
+    }
+
+}
+
